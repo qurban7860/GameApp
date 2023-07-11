86f2ffc1ed74066191fa59f814c126a11b66a5cb
SL Laptop <m85150765@gmail.com>
2023-07-11 16:34:04 +0500

    Connection done

---
+++ app/src/main/java/com/example/playgame/DisplayFragment.java
@@ -0,0 +1,81 @@
+package com.example.playgame;
+
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisplayFragment extends Fragment {
+
+    private List<Result> testResultList;
+    private ResultAdapter testResultAdapter;
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Initialize the list and adapter
+        testResultList = new ArrayList<>();
+        testResultAdapter = new ResultAdapter(getActivity(), testResultList);
+        databaseHelper = new DatabaseHelper(getActivity());
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.display_fragment, container, false);
+
+        ListView listView = view.findViewById(R.id.listview);
+        listView.setAdapter(testResultAdapter);
+
+        // Retrieve data from the database
+        retrieveData();
+
+        return view;
+    }
+
+    private void retrieveData() {
+        // Clear the list
+        testResultList.clear();
+
+        // Retrieve data from the database
+        SQLiteDatabase db = databaseHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + DatabaseHelper.TABLE_RESULTS, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                // Extract the data from the cursor
+                @SuppressLint("Range") int id = cursor.getInt(cursor.getColumnIndex(DatabaseHelper.COLUMN_ID));
+                @SuppressLint("Range") String questions = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_QUESTIONS));
+                @SuppressLint("Range") String selections = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_SELECTIONS));
+                @SuppressLint("Range") String correctAnswers = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_CORRECT_ANSWERS));
+                @SuppressLint("Range") int score = cursor.getInt(cursor.getColumnIndex(DatabaseHelper.COLUMN_SCORE));
+
+                // Create a TestResult object
+                Result testResult = new Result(id,questions, selections, correctAnswers, score);
+
+                // Add the TestResult object to the list
+                testResultList.add(testResult);
+
+            } while (cursor.moveToNext());
+        }
+
+        // Close the cursor and database connection
+        if (cursor != null) {
+            cursor.close();
+        }
+        db.close();
+
+        // Notify the adapter that the data has changed
+        testResultAdapter.notifyDataSetChanged();
+    }
+}
+
---
+++ app/src/main/java/com/example/playgame/Result.java
@@ -0,0 +1,40 @@
+package com.example.playgame;
+
+public class Result {
+    private int id;
+    private String questions;
+    private String selections;
+    private String correctAnswers;
+
+    private int score;
+
+    public Result(int id, String questions, String selections, String correctAnswers,  int score) {
+        this.id = id;
+        this.questions = questions;
+        this.selections = selections;
+        this.correctAnswers = correctAnswers;
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getQuestions() {
+        return questions;
+    }
+
+    public String getSelections() {
+        return selections;
+    }
+
+    public String getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
+
+
